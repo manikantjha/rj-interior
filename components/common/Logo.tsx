f0b805aba062b8eff5c2c04d1651d2b6f0b7c4b3
manikantjha <being.manikantjha@gmail.com>
2023-03-27 01:26:22 +0530

    Fixed navbar logo text spawning two lines.

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
         className="md:h-12 mr-3 h-10"
         alt="Flowbite Logo"
       />
-      <h1 className="text-xl font-bold mt-2">RJ Interior</h1>
+      <h1 className="text-xl font-bold mt-2 flex-shrink-0">RJ Interior</h1>
     </div>
   );
 }
