fd4e8aa70d0f09b4801dd1e9c2c018c16c3cbdfa
manikantjha <being.manikantjha@gmail.com>
2023-03-27 01:22:08 +0530

    Fixed navbar logo.

--- layout/navBar/NavBar.tsx
+++ layout/navBar/NavBar.tsx
@@ -15,7 +15,7 @@ export default function NavBar() {
   return (
     <>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 shadow-md sticky top-0 left-0 right-0 z-[1000]">
-        <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <div className="container grid grid-cols-[1fr_auto] items-center mx-auto">
           <Link href="/" className="flex items-center">
             <Logo />
           </Link>
