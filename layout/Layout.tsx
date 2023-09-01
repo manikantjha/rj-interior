f1a65b764d3b8f622f6f76e086faff60d003825b
manikantjha <being.manikantjha@gmail.com>
2023-09-01 23:46:26 +0530

    Moved whatsapp btn to client layout.

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./navbar/Navbar";
+import WhatsAppButton from "@/components/common/WhatsAppButton";
 
 interface IMainLayout {
   children: ReactNode;
