5b59385c99b29f5ba972c1e479011489d2be2227
manikantjha <being.manikantjha@gmail.com>
2023-09-01 23:45:24 +0530

    Moved whatsapp btn to client layout.

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -11,6 +11,7 @@ export default function Layout(props: IMainLayout) {
     <>
       <Navbar />
       <>{props.children}</>
+      <WhatsAppButton />
       <Footer />
     </>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-      <WhatsAppButton />
       <MoveToTopButton />
     </QueryClientProvider>
   );
